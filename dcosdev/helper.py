24da6518a5e4532e256c917ea3511c381a21d289
srajappa <srajappa@buffalo.edu>
2020-02-10 10:54:33 -0500

    FIX: Resolving Conversations in PR #1 1. Uncommenting statements in `commands.py`; the problem in the statement was identified and edited. 2. Author clarified that Python 3.* is being used and that `README.md` wasn't updated properly; the changes are undone.

--- dcosdev/helper.py
+++ dcosdev/helper.py
@@ -42,13 +42,7 @@ def _read_config_values(artifacts_url):
     if os.path.exists("config.yml"):
         with open("config.yml") as f:
             config_values = yaml.safe_load(f.read()).get("values", dict())
-    
-    # Only works with Python 3.*
-    # return {**config_values, **global_values}
-
-    # For Python 2.*
-    config_values.update(global_values)
-    return config_values
+    return {**config_values, **global_values}
 
 def _prerender_file(config_values, filename):
     with open(filename) as f:
